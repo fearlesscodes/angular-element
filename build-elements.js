ddf9cbe94f644f06bf08d09665f85d3ae3b4ae80
Anton Truong <antont86@gmail.com>
2019-08-23 08:51:49 +0200

    Working angular element without routing

---
+++ build-elements.js
@@ -0,0 +1,14 @@
+const fs = require('fs-extra');
+const concat = require('concat');
+
+(async function build() {
+  const files = [
+    './dist/hello-without-router/runtime-es2015.js',
+    './dist/hello-without-router/polyfills-es2015.js',
+    './dist/hello-without-router/scripts.js',
+    './dist/hello-without-router/main-es2015.js'
+  ];
+
+  await fs.ensureDir('elements');
+  await concat(files, 'elements/hello-app.js');
+})();
